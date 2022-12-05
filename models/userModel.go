e9d689b19d13bec8a735d5b62fddfaf159d2ca24
fajar hidayat <fajarh247@gmail.com>
2022-12-05 21:45:19 +0700

    enhance login response v4

--- models/userModel.go
+++ models/userModel.go
@@ -1,12 +1,17 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	// "gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Fullname 	string 	`json:"fullname"`
-	Age 		int16 	`json:"age"`		
-	PhoneNumber string 	`json:"phonenumber"`
-	Email 		string 	`json:"email"`	
-	Password 	string	`json:"password"`	
+	CreatedAt   time.Time	`json:"createdat"`
+  	UpdatedAt   time.Time	`json:"updatedat"`
+	Fullname 	string 		`json:"fullname"`
+	Age 		int16 		`json:"age"`		
+	PhoneNumber string 		`json:"phonenumber"`
+	Email 		string 		`json:"email"`	
+	Password 	string		`json:"password"`	
 }
