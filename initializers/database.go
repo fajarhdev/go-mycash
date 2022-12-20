25353066faefe8033aecce4c6641b25232057bf2
fajar hidayat <fajarh247@gmail.com>
2022-12-20 07:04:20 +0700

    bug fixing transaction v3

--- initializers/database.go
+++ initializers/database.go
@@ -12,7 +12,7 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "root:@tcp(127.0.0.1:3306)/mycash?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:root@tcp(127.0.0.1:3306)/mycash?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database")
